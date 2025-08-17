e9b55a90649d3693eaa94ee7e5419edb33619c1b
catasteria <catasteria@gmail.com>
2025-08-17 08:46:27 -0600

    movimiento coches

--- rain-rider/bomba.gd
+++ rain-rider/bomba.gd
@@ -1,7 +1,13 @@
 extends StaticBody2D
 
-@onready var sonido = $AudioStreamPlayer2D
+@onready var explosion = $AudioStreamPlayer2D
+@onready var bomba = $AudioStreamPlayer2D2
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
-		sonido.play()
+		explosion.play()
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("jugador"):
+		bomba.play()
