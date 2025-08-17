802bde6eda631224083d54c1e157cce5af6fc3d6
ElSedim <elsedim09@gmail.com>
2025-08-16 18:24:06 -0600

    Correcciones
    Pequeñas correcciones y añadido de sonido de gota cuando colisiona contra el player

--- rain-rider/gota_lluvia.gd
+++ rain-rider/gota_lluvia.gd
@@ -5,27 +5,36 @@ var velocity : Vector2 = Vector2.ZERO
 var lifeTime : float = 5.0
 var timeStart : float = 0.0
 
+@onready var sound : AudioStreamPlayer = $WaterHit
+
+var hit : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	velocity = Vector2.ZERO
 	timeStart = Time.get_ticks_msec()
 	pass 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	velocity.y += delta * 9.81
-	global_position += velocity
-	var current_time = Time.get_ticks_msec()
-	var elapsed_time = (current_time - timeStart) / 1000.0
-	if elapsed_time >= lifeTime - 1 :
-		#print("liberar gota")
-		queue_free()
+	if hit == false:
+		velocity.y += delta * 9.81
+		global_position += velocity
+		var current_time = Time.get_ticks_msec()
+		var elapsed_time = (current_time - timeStart) / 1000.0
+		if elapsed_time >= lifeTime - 1 :
+			queue_free()
+	else:
+		if sound.playing == false:
+			queue_free()
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		print("gota toco jugador")
+	if body is Player and hit == false:
+		$CollisionShape2D.disabled = true
+		sound.play()
 		body.DecreaseLive(1)
-		queue_free()
+	#queue_free()
+	hit = true
+	visible = false	
 	pass
