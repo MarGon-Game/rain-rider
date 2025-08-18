526ec03f17749c6a6a3b67353de9fe251fac7513
catasteria <catasteria@gmail.com>
2025-08-17 22:11:54 -0600

    nivel2 bombas

--- rain-rider/pasar_nivel.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("jugador"):   # asegúrate que tu Player está en este grupo
-		get_tree().change_scene_to_file("res://nivel_2.tscn")
